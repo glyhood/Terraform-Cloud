6532ccb2ce098a1a8e132d2934248fd88aba839f
IfeDevOps <43143513+glyhood@users.noreply.github.com>
2023-02-05 03:47:59 +0100

    Update gitlab.tf

--- terraform-cloud-main/production/eu-west-3/gitlab/gitlab.tf
+++ terraform-cloud-main/production/eu-west-3/gitlab/gitlab.tf
@@ -11,3 +11,18 @@ resource "gitlab_group_variable" "argocd_gat" {
   key   = "argocd_gat"
   value = gitlab_group_access_token.xxxx.token
 }
+
+locals {
+  gitlab_repos = {
+    testrepo1 = { description = "example testrepo1" }
+    testrepo2 = { description = "example testrepo2" }
+    # testrepo3 = { project_id = 6166, description = "" }
+  }
+}
+
+module "gitlab_project" {
+  source = "../gitlab_module"
+  for_each    = local.gitlab_repos
+  name        = "${each.key}"
+  description = local.gitlab_repos[each.key].description
+}
