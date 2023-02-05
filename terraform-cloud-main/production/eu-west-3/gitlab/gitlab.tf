9d60055917f17320dc774730eb6295cecfa583e8
IfeDevOps <43143513+glyhood@users.noreply.github.com>
2023-02-05 13:00:03 +0100

    Update gitlab.tf

--- terraform-cloud-main/production/eu-west-3/gitlab/gitlab.tf
+++ terraform-cloud-main/production/eu-west-3/gitlab/gitlab.tf
@@ -14,15 +14,28 @@ resource "gitlab_group_variable" "argocd_gat" {
 
 locals {
   gitlab_repos = {
-    testrepo1 = { description = "example testrepo1" }
-    testrepo2 = { description = "example testrepo2" }
+    testrepo1 = { description = "example testrepo1", group = module.gitlab_group["testgroup1"].id }
+    testrepo2 = { description = "example testrepo2", group = module.gitlab_group["testgroup2"].id }
     # testrepo3 = { project_id = 6166, description = "" }
+  }
+  gitlab_groups = {
+    testgroup1 = { description = "example testgroup1", path = "group/devops"}
+    testgroup2 = { description = "example testgroup2", path = "group/devops"}
   }
 }
 
 module "gitlab_project" {
-  source = "../gitlab_module"
+  source = "../gitlab_modules/projects"
   for_each    = local.gitlab_repos
   name        = "${each.key}"
   description = local.gitlab_repos[each.key].description
+  namespace = local.gitlab_repos[each.key].group
+}
+
+module "gitlab_group" {
+  source = "../gitlab_modules/groups"
+  for_each    = local.gitlab_groups
+  name        = "${each.key}"
+  description = local.gitlab_groups[each.key].description
+  path = local.gitlab_groups[each.key].path
 }
